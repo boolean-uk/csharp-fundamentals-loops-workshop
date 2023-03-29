7d0cba7cd92739685575f81c0687a9e475330f32
uerbzr <nigel.sibbert@gmail.com>
2023-03-29 10:46:13 +0100

    Add project files.

---
+++ workshop.Main/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace workshop.Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //up
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //down
+            for (int j = 100; j > 0; j--)
+            {
+
+                Console.WriteLine(j);
+            }
+
+
+            //up
+            for (int k = 0; k < 1000; k=k+5)
+            {
+                Console.WriteLine(k);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
