99837113bee12f95279b2cbe94139b7e4ecad582
uerbzr <nigel.sibbert@gmail.com>
2023-09-01 10:53:27 +0100

    update

--- workshop.Main/Program.cs
+++ workshop.Main/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            
+     
+
             //while loop
             int i = 10;
             while (i < 10)
@@ -12,6 +15,14 @@
                 i++;
             }
 
+            //beware of the infinite while!            
+            int aVariableThatNeverIncrements = 1;
+            while (aVariableThatNeverIncrements < 10)
+            {
+                Console.WriteLine($"The counter is {aVariableThatNeverIncrements}");
+                bool result = 1 == 1 ? 1==2 ? false : true : false;
+
+            }
 
             //do while
             int j = 10;
@@ -19,7 +30,7 @@
             {
                 Console.WriteLine($"The counter is {j}");
                 j++;
-            } while (j < 10);
+            } while (j < 1);
 
 
 
@@ -31,26 +42,32 @@
 
 
             //up
-            for (int i = 0; i < 10; i++)
+            for (int l = 0; l < 10; l++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(l);
             }
 
 
             //down
-            for (int j = 100; j > 0; j--)
+            for (int m = 100; m > 0; m--)
             {
 
-                Console.WriteLine(j);
+                Console.WriteLine(m);
             }
 
 
-            //up
-            for (int k = 0; k < 1000; k=k+5)
+            //upagain
+            for (int n = 0; n < 1000; n=n+5)
             {
-                Console.WriteLine(k);
+                Console.WriteLine(n);
             }
 
+            //foreach
+            int[] intArray = { 1, 2, 3 };
+            foreach (int o in intArray)
+            {
+                Console.WriteLine(o);
+            }
 
             Console.ReadLine();
         }
